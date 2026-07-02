6c977926c602c7c17f11e18d534b3d39d58838fd
ju4nko <ju4nko@gmail.com>
2026-07-02 09:51:45 +0200

    Initial Commit

---
+++ BlackjackGame/BlackjackGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackjackGameApp.swift
+//  BlackjackGame
+//
+//  Created by Juanjo on 02/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct BlackjackGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
